fc62fbd2cefe6574efcae1685ccb198bf119d772
Aung Pyae Phyo <115131869+aungpyaephyo1412@users.noreply.github.com>
2024-05-06 09:23:28 +0630

    init: project initilization :zap:

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Data.SqlClient;
+
+Console.WriteLine("Hello, World!");
+
+SqlConnectionStringBuilder stringBuilder = new SqlConnectionStringBuilder();
+stringBuilder.DataSource = ".";
+stringBuilder.InitialCatalog = "DotNetSLH";
+stringBuilder.UserID = "sa";
+stringBuilder.Password = "Typle@14122003";
+
+SqlConnection connection = new SqlConnection(stringBuilder.ConnectionString);
+
+connection.Open();
+Console.WriteLine("Connection Open.");
+
+connection.Close();
+Console.WriteLine("Connection Close.");
+
+Console.ReadKey();
