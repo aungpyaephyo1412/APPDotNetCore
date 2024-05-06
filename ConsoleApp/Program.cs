aa2f7a3a1984db6d012cc84977a12bca26583203
Aung Pyae Phyo <115131869+aungpyaephyo1412@users.noreply.github.com>
2024-05-06 16:56:12 +0630

    ADO read

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,11 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using System.Data;
 using System.Data.SqlClient;
 
 Console.WriteLine("Hello, World!");
 
-SqlConnectionStringBuilder stringBuilder = new SqlConnectionStringBuilder();
-stringBuilder.DataSource = ".";
+SqlConnectionStringBuilder stringBuilder = new SqlConnectionStringBuilder(); // Default value null
+stringBuilder.DataSource = "."; /* Server Name */
 stringBuilder.InitialCatalog = "DotNetSLH";
 stringBuilder.UserID = "sa";
 stringBuilder.Password = "Typle@14122003";
@@ -15,7 +16,20 @@ SqlConnection connection = new SqlConnection(stringBuilder.ConnectionString);
 connection.Open();
 Console.WriteLine("Connection Open.");
 
+string query = "SELECT * FROM TblBlog";
+SqlCommand cmd = new SqlCommand(query, connection);
+SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(cmd);
+DataTable dataTable = new DataTable();
+sqlDataAdapter.Fill(dataTable);
 connection.Close();
 Console.WriteLine("Connection Close.");
+
+foreach (DataRow dataRow in dataTable.Rows)
+{
+    Console.WriteLine(dataRow["BlogId"]);
+    Console.WriteLine(dataRow["BlogTitle"]);
+    Console.WriteLine(dataRow["BlogAuthor"]);
+    Console.WriteLine(dataRow["BlogContent"]);
+}
 
 Console.ReadKey();
