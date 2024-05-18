9053c45d10b6e4c8cb0957c45ca7d888982bc996
Aung Pyae Phyo <115131869+aungpyaephyo1412@users.noreply.github.com>
2024-05-18 20:07:31 +0630

    Web api with dapper crud

---
+++ RestApi/Controllers/BlogDapperController.cs
@@ -0,0 +1,122 @@
+using System.Data;
+using Dapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using RestApi.Models;
+
+namespace RestApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class BlogDapperController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult GetBlogs()
+    {
+        string query = "select * from Tbl_Blog";
+        using IDbConnection db = new SqlConnection(ConnectionString.SqlConnectionStringBuilder.ConnectionString);
+        List<BlogModel> list = db.Query<BlogModel>(query).ToList();
+        return Ok(list);
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult GetBlog(int id)
+    {
+        var item = FindById(id);
+        if (item is null)
+        {
+            return NotFound();
+        }
+
+        return Ok();
+    }
+
+    [HttpPost]
+    public IActionResult CreateBlog(BlogModel blog)
+    {
+        string query =
+            @"INSERT INTO Tbl_Blog ([BlogTitle],[BlogAuthor],[BlogContent]) VALUES (@blogTitle,@blogAuthor,@blogContent)";
+        using IDbConnection db = new SqlConnection(ConnectionString.SqlConnectionStringBuilder.ConnectionString);
+        db.Execute(query, blog);
+        return Ok();
+    }
+
+    [HttpPut("{id}")]
+    public IActionResult UpdateBlog(int id, BlogModel blog)
+    {
+        string query =
+            @"UPDATE Tbl_Blog SET [BlogTitle] = @BlogTitle,[BlogAuthor] = @BlogAuthor,[BlogContent]=@BlogContent WHERE BlogId=@BlogId";
+        using IDbConnection db = new SqlConnection(ConnectionString.SqlConnectionStringBuilder.ConnectionString);
+        var item = FindById(id);
+        if (item is null)
+        {
+            return NotFound();
+        }
+
+        blog.BlogId = id;
+        db.Execute(query, blog);
+        return Ok();
+    }
+
+    [HttpPatch("{id}")]
+    public IActionResult PathBlog(int id, BlogModel blog)
+    {
+        using IDbConnection db = new SqlConnection(ConnectionString.SqlConnectionStringBuilder.ConnectionString);
+        var item = FindById(id);
+        if (item is null)
+        {
+            return NotFound();
+        }
+
+        string conditions = string.Empty;
+        if (!string.IsNullOrEmpty(blog.BlogTitle))
+        {
+            conditions += "[BlogTitle] = @BlogTitle,";
+        }
+
+        if (!string.IsNullOrEmpty(blog.BlogAuthor))
+        {
+            conditions += "[BlogAuthor] = @BlogAuthor,";
+        }
+
+        if (!string.IsNullOrEmpty(blog.BlogContent))
+        {
+            conditions += "[BlogContent] = @BlogContent,";
+        }
+
+        if (conditions.Length == 0)
+        {
+            return NotFound();
+        }
+
+        conditions = conditions.Substring(conditions.Length - 2);
+        string query =
+            $@"UPDATE Tbl_Blog SET {conditions}";
+        blog.BlogId = id;
+        db.Execute(query, blog);
+        return Ok();
+    }
+
+    [HttpDelete("{id}")]
+    public IActionResult DeleteBlog(int id)
+    {
+        var item = FindById(id);
+        if (item is null)
+        {
+            return NotFound();
+        }
+
+        string query = @"DELETE FROM Tbl_Blog WHERE [BlogId]=@BlogId";
+
+        using IDbConnection db = new SqlConnection(ConnectionString.SqlConnectionStringBuilder.ConnectionString);
+        db.Execute(query, new BlogModel { BlogId = id });
+        return NoContent();
+    }
+
+    private BlogModel? FindById(int id)
+    {
+        string query = "select * from Tbl_Blog where id = @BlogId";
+        using IDbConnection db = new SqlConnection(ConnectionString.SqlConnectionStringBuilder.ConnectionString);
+        return db.Query(query, new BlogModel { BlogId = id }).FirstOrDefault();
+    }
+}
