995ec754715179f37f194e93e8cda7f3fc81ecfe
Aung Pyae Phyo <115131869+aungpyaephyo1412@users.noreply.github.com>
2024-05-27 06:25:42 +0630

    Day 20

--- ConsoleAppWithHTTPClientExample/HttpClientExample.cs
+++ ConsoleAppWithHTTPClientExample/HttpClientExample.cs
@@ -1,3 +1,5 @@
+using System.Net.Mime;
+using System.Text;
 using Newtonsoft.Json;
 
 namespace ConsoleAppWithHTTPClientExample;
@@ -28,5 +30,72 @@ public class HttpClientExample
                 Console.WriteLine(li);
             }
         }
+    }
+
+    private async void Edit(int id)
+    {
+        var result = await _client.GetAsync($"/Blog/{id}");
+        if (result.IsSuccessStatusCode)
+        {
+            string jsonStr = await result.Content.ReadAsStringAsync();
+            BlogModel item = JsonConvert.DeserializeObject<BlogModel>(jsonStr)!;
+            Console.WriteLine(item);
+            return;
+        }
+
+        string message = await result.Content.ReadAsStringAsync();
+        Console.WriteLine(message);
+    }
+
+    private async void Delete(int id)
+    {
+        var result = await _client.DeleteAsync($"/Blog/{id}");
+        if (result.IsSuccessStatusCode)
+        {
+            string jsonStr = await result.Content.ReadAsStringAsync();
+            Console.WriteLine(jsonStr);
+            return;
+        }
+
+        string message = await result.Content.ReadAsStringAsync();
+        Console.WriteLine(message);
+    }
+
+    private async void Create(string title, string author, string content)
+    {
+        BlogModel blogModel = new BlogModel()
+        {
+            BlogTitle = title,
+            BlogAuthor = author,
+            BlogContent = content
+        };
+        HttpContent httpContent = new StringContent(JsonConvert.SerializeObject(blogModel), Encoding.UTF8,
+            MediaTypeNames.Application.Json);
+        var response = await _client.PostAsync("/blog", httpContent);
+        if (response.IsSuccessStatusCode)
+        {
+            Console.WriteLine(response.Content.ReadAsStringAsync());
+        }
+
+        Console.WriteLine(response.Content.ReadAsStringAsync());
+    }
+
+    private async void Update(int id, string title, string author, string content)
+    {
+        BlogModel blogModel = new BlogModel()
+        {
+            BlogTitle = title,
+            BlogAuthor = author,
+            BlogContent = content
+        };
+        HttpContent httpContent = new StringContent(JsonConvert.SerializeObject(blogModel), Encoding.UTF8,
+            MediaTypeNames.Application.Json);
+        var response = await _client.PostAsync($"/blog{id}", httpContent);
+        if (response.IsSuccessStatusCode)
+        {
+            Console.WriteLine(response.Content.ReadAsStringAsync());
+        }
+
+        Console.WriteLine(response.Content.ReadAsStringAsync());
     }
 }
